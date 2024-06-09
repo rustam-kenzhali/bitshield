35f545b23619d899ffca0b3752110355df550585
Rustam Kenzhali <74444247+rustam-kenzhali@users.noreply.github.com>
2024-06-09 20:33:19 +0500

    Delete Telegram Bot finall version directory

--- Telegram Bot finall version/bot_config.py
+++
@@ -1,3 +0,0 @@
-import telebot
-# Initialize the bot with your token
-bot = telebot.TeleBot('7010154353:AAGXiZXQh6_6cJ2YBxdN9tutYNfe1JCfV0g')
--- Telegram Bot finall version/main.py
+++
@@ -1,7 +0,0 @@
-from handels import run_bot
-from database import init_db
-
-
-if __name__ == '__main__':
-    init_db()
-    run_bot()
